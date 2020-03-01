9f07bb1665546af6c3ed20cf4fe8dac451593177
Arun <sarun6153@gmail.com>
2020-03-01 18:30:30 +0530

    password hasing implemented and signup is working

--- serverAuth/authApp/models.py
+++ serverAuth/authApp/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from passlib.hash import pbkdf2_sha256
 
 class User(models.Model):
     userid=models.CharField(max_length=20)
     name=models.CharField(max_length=30)
     email=models.CharField(max_length=30)
-    password=models.CharField(max_length=15)
+    password=models.CharField(max_length=256)
     phone=models.CharField(max_length=10)
     option=models.CharField(max_length=7,null=True, blank=True)
--- serverAuth/authApp/views.py
+++ serverAuth/authApp/views.py
@@ -2,11 +2,58 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotFound, JsonResponse, HttpResponseNotAllowed
 from django.db import connection
 from .models import User
-import json,jwt
+from django.contrib.auth.hashers import make_password, check_password
+import json
+import jwt
 
 
-def login():
-    print("in login")
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        values = json.loads(request.body.decode('utf-8'))
+        email = values['email']
+        password = values['password']
+        try:
+            user = User.objects.get(email=f'{email}')
+            if user.password == password:
+                payload = {
+                    'email': user.email,
+                }
+                jwt_token = jwt.encode(
+                    payload, "WANNA_BREACH", algorithm='HS256',)
+                data = {
+                    'status': '200',
+                    'token': f'{jwt_token}',
+                    'email': f'{user.email}'
+                }
+                return JsonResponse(data)
+        except Exception as e:
+            return HttpResponseNotFound("User not found")
+    else:
+        return HttpResponseNotAllowed('<p>Not allowed</p>')
 
-def signup():
-    print("in logout")
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        values = json.loads(request.body.decode('utf-8'))
+
+        name        = values['name']
+        email       = values['email']
+        userid      = values['userID']
+        passRaw     = values['password']
+        phone       = values['phone']
+        option      = values['option']
+        
+        passwordEn = make_password(passRaw)
+        # passwordEn="pbkdf2_sha256$180000$18b5QY0u3b38$p9C+fAl0jBJMt4VmH60x1jWb4Cj4eOsPo+Gwvxhw8P4="
+        # print("Hashed Password:- "+passwordEn)
+        # print(check_password(passRaw,passwordEn))
+        newUser = User(name=name, email=email, userid=userid, phone=phone, password=passwordEn, option=option)
+        newUser.save()
+        return HttpResponse(status=201)
+
+    else:
+        return HttpResponseNotAllowed('<p>Not allowed</p>')
+
+# pbkdf2_sha256$180000$18b5QY0u3b38$p9C+fAl0jBJMt4VmH60x1jWb4Cj4eOsPo+Gwvxhw8P4=
+# pbkdf2_sha256$180000$Ep2wdSi9ldbK$Hk5we+6dY2b4DZJLBauGaYIdUkPR+T5TjBCliuzXom0=
