5ba77ab3ba8dcd598a158d6c295d45d9901cfb7d
Arun <sarun6153@gmail.com>
2020-03-04 00:47:56 +0530

    sorting is working.

--- src/app/list-users/list-users.component.ts
+++ src/app/list-users/list-users.component.ts
@@ -17,21 +17,78 @@ export class ListUsersComponent implements OnInit {
     this.bringAllUsers();
   }
 
+  myFunction(event) {
+    let input, filter, table, tr, td, i, txtValue;
 
-  bringAllUsers()
-  {
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
+  sortTable(n) {
+    let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("myTable");
+    switching = true;
+    dir = "asc";
+    while (switching) {
+      switching = false;
+      rows = table.rows;
+      for (i = 1; i < (rows.length - 1); i++) {
+        shouldSwitch = false;
+        x = rows[i].getElementsByTagName("TD")[n];
+        y = rows[i + 1].getElementsByTagName("TD")[n];
+        if (dir == "asc") {
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            shouldSwitch = true;
+            break;
+          }
+        } else if (dir == "desc") {
+          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+      if (shouldSwitch) {
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        switchcount++;
+      } else {
+        if (switchcount == 0 && dir == "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }
+  }
+
+  bringAllUsers() {
     this.list.list().subscribe(
-      (res)=>{
+      (res) => {
         this.fetchedUsers = res.data;
       }
     )
   }
+
+
   ///  CSV EXPORT IS WORKING HERE
   exportCSV() {
-        let a = document.createElement('a');
-        a.href = "http://127.0.0.1:8000/download-csv";
-        a.target ='_blank';
-        a.download = 'edit_user.csv';
-        a.click();
+    let a = document.createElement('a');
+    a.href = "http://127.0.0.1:8000/download-csv";
+    a.target = '_blank';
+    a.download = 'edit_user.csv';
+    a.click();
   }
 }
