085b53523f25c8d644a0cb71e9e0db65518237df
Arun <sarun6153@gmail.com>
2020-03-04 01:04:18 +0530

    searching and sortng is enabled.

--- src/app/list-users/list-users.component.ts
+++ src/app/list-users/list-users.component.ts
@@ -18,18 +18,33 @@ export class ListUsersComponent implements OnInit {
   }
 
   myFunction(event) {
-    let input, filter, table, tr, td, i, txtValue;
+    let input, filter, table, tr, td1, td2, td3, td4, i, txtValue1, txtValue2, txtValue3, txtValue4;
 
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
     table = document.getElementById("myTable");
     tr = table.getElementsByTagName("tr");
-    
+
     for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      td1=  tr[i].getElementsByTagName("td")[0];
+      td2=  tr[i].getElementsByTagName("td")[1];
+      td3=  tr[i].getElementsByTagName("td")[2];
+      td4=  tr[i].getElementsByTagName("td")[3];
+
+
+      if (td1 || td2 || td3 || td4) {
+        txtValue1 = td1.textContent || td1.innerText;
+        txtValue2 = td2.textContent || td2.innerText;
+        txtValue3 = td3.textContent || td3.innerText;
+        txtValue4 = td4.textContent || td4.innerText;
+
+        let one = txtValue1.toUpperCase().indexOf(filter);
+        let two = txtValue2.toUpperCase().indexOf(filter);
+        let three = txtValue3.toUpperCase().indexOf(filter);
+        let four = txtValue4.toUpperCase().indexOf(filter);
+
+
+        if ((one > -1)|| (two > -1) ||(three > -1) ||(four > -1)) {
           tr[i].style.display = "";
         } else {
           tr[i].style.display = "none";
